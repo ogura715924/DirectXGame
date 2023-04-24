f2b1ed0179449afe145fa49267a66369643af784
81906 <k022g0083@g.neec.ac.jp>
2023-04-24 12:32:50 +0900

    a

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,53 +1,22 @@
 #include "GameScene.h"
-#include "TextureManager.h"
+#include "AxisIndicator.h"
 #include "ImGuiManager.h"
-#include"PrimitiveDrawer.h"
-#include"AxisIndicator.h"
+#include "PrimitiveDrawer.h"
+#include "TextureManager.h"
 #include <cassert>
-#include"Model.h"
-#include"WorldTransform.h"
-#include"Model.h"
-#include"WorldTransform.h"
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {
-delete model_;
-	delete player_;
-delete model_;
-}
+GameScene::~GameScene() {}
 
 void GameScene::Initialize() {
 
 	dxCommon_ = DirectXCommon::GetInstance();
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
-
-	texHandle_ = TextureManager::Load("debugfont.png");
-
-	//モデル
-	viewProjection_.Initialize();
-	model_ = Model::Create();
-
-	// ファイル名を指定してテクスチャを読み込む
-	textureHandle_ = TextureManager::Load("mario.jpg");
-	// 3Dモデルの生成
-	model_ = Model::Create();
-	// ビュープロジェクションの初期化
-	viewProjection_.Initialize();
-	// ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
-	PrimitiveDrawer::GetInstance()->SetViewProjection(&viewProjection_);
-	//自キャラの生成
-	player_ = new Player();
-	//自キャラの初期化
-	player_->Initialize(model_,textureHandle_);
 }
 
-void GameScene::Update() {
-	//自キャラの更新
-	player_->Update();
-	}
-	
+void GameScene::Update() {}
 
 void GameScene::Draw() {
 
@@ -55,7 +24,6 @@ void GameScene::Draw() {
 	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
 
 #pragma region 背景スプライト描画
-	
 
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
@@ -70,14 +38,11 @@ void GameScene::Draw() {
 #pragma region 3Dオブジェクト描画
 	// 3Dオブジェクト描画前処理
 	Model::PreDraw(commandList);
-	//自キャラの描画
-	player_->Draw(viewProjection_);
 
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-	
-	
+
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
@@ -89,7 +54,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-	
+
 	// スプライト描画後処理
 	Sprite::PostDraw();
 
