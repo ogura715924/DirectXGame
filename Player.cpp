66e9106f24bf303d8b25f251d3279aaa76ce4e2b
81906 <k022g0083@g.neec.ac.jp>
2023-04-25 12:58:02 +0900

    a

--- Player.cpp
+++ Player.cpp
@@ -11,15 +11,40 @@ void Player::Initialize(Model*model,uint32_t textureHandle) {
 
 	//ワールドトランスフォームの初期化
 	worldTransform_.Initialize();
+
+	// シングルトンインスタンスを取得する
+	input_ = Input::GetInstance();
 }
 void Player::Update(){
 	//行列を定数バッファに転送
 	worldTransform_.TransferMatrix();
+
+	//キャラクターの移動ベクトル
+	Vector3 move = {0, 0, 0};
+	//キャラクターの移動の速さ
+	const float kCharacterSpeed = 0.2f;
+
+	// 押した方向でベクトルを変更(左右)
+	//if (input_ -= Pushkey(DIK_LEFT)) {
+	//	move.x -= kCharacterSpeed;
+	//} else if (input_->PushKey(DIK_RIGHT)) {
+	//	move.x += kCharacterSpeed;
+	//}
+	//// 押した方向でベクトルを変更(上下)
+	//if (input_ -= Pushkey(DIK_UP)) {
+	//	move.y -= kCharacterSpeed;
+	//} else if (input_->PushKey(DIK_DOWN)) {
+	//	move.y += kCharacterSpeed;
+	//}
+
+	////座標移動(ベクトルの加算)
+	//worldTransform_.translation_ += move;
+
 }
+
 void Player::Draw(ViewProjection&ViewProjection) {
 	//3Dモデルを描画
 	model_->Draw(worldTransform_, ViewProjection, textureHandle_);
 
-	//シングルトンイン巣探査うを取得する
-	input_ = Input::GetInstance();
+	
 }
